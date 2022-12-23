81ec2625c2f79959872167e3b65e75cacae94154
BrahimZ7 <Miritoni1>
2022-12-23 11:45:08 +0100

    Refactoring

---
+++ src/main/java/at/fhtw/swen3/gps/service/impl/OpenStreetProxy.java
@@ -0,0 +1,9 @@
+package at.fhtw.swen3.gps.service.impl;
+
+import at.fhtw.swen3.gps.service.GeoEncodingService;
+
+public class OpenStreetProxy implements GeoEncodingService {
+    public OpenStreetProxy() {
+        super();
+    }
+}
--- src/main/java/at/fhtw/swen3/services/ParcelService.java
+++ src/main/java/at/fhtw/swen3/services/ParcelService.java
@@ -1,4 +1,10 @@
 package at.fhtw.swen3.services;
 
+import at.fhtw.swen3.services.dto.Parcel;
+
 public class ParcelService {
+    public String getStorage(Parcel p) {
+        return 
+    }
+
 }
